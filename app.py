1a6579d2df80d8faf11cc087425bcaa42783dabc
Xthe23 <michaelkravitz98@gmail.com>
2024-01-11 20:10:06 -0500

    Update app.py with routing logic for MongoDB Wildfires data

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask
+from pymongo import MongoClient
+import json
+from flask import jsonify
+
+app = Flask(__name__)
+
+# Connect to MongoDB
+client = MongoClient('mongodb://localhost:27017/')
+db = client['wildfires_db']
+collection = db['wildfires']
+
+
+def load_data():
+    file_path = './Resources/Wildfires 2.json'
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+        # Check if data is already loaded in the database to avoid duplicate entries
+        if collection.count_documents({}) == 0:
+            if isinstance(data, list):  # Check if data is a list of records
+                collection.insert_many(data)  # Use insert_many for list
+            else:
+                # Use insert_one for a single record
+                collection.insert_one(data)
+        else:
+            print("Data already loaded in the database.")
+    return
+
+
+load_data()
+
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1/wildfires - List wildfires data<br/>"
+    )
+
+
+@app.route('/api/v1/wildfires')
+def get_wildfires_data():
+    # Retrieve data from MongoDB
+    data_cursor = collection.find().limit(5)
+
+    # Convert cursor to list
+    data_list = list(data_cursor)
+
+    # Convert ObjectId to string, because MongoDB uses ObjectId type and JSON does not support it
+    for document in data_list:
+        document['_id'] = str(document['_id'])
+
+    return jsonify(data_list)
+
+
+if __name__ == '__main__':
+    app.run()
